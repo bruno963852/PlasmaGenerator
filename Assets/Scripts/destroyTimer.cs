c63f57cbf7ca5c3e632c94c635adb556541d3b22
Bruno Martins <bruno963852@gmail.com>
2014-03-26 21:59:24 -0300

    Inimigo agora explode com tiro!

---
+++ Assets/Scripts/destroyTimer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyTimer : MonoBehaviour {
+
+	public float time = 1;
+
+	private float updateTime;
+
+	// Use this for initialization
+	void Start () 
+	{
+		updateTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (updateTime + time <= Time.time)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/enemyDestroyscript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyDestroyscript : MonoBehaviour {
+
+	public GameObject explosion;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag.Equals("Bullet"))
+		{
+			Instantiate(explosion, transform.position, Quaternion.identity);
+			Destroy(this.gameObject);
+		}
+	}
+}
